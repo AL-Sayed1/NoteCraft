0772d2cc458c30b4d82f4eb94cd2e7c790e15915
AL-Sayed1 <153854740+AL-Sayed1@users.noreply.github.com>
2024-12-04 06:59:04 +0000

    Updated Ask PDF to support google gemini

--- pages/4_🔍_Ask_PDF.py
+++ pages/4_🔍_Ask_PDF.py
@@ -7,30 +7,37 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 import utils
 from langchain_community.chat_models import ChatOpenAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
 
-
-def get_text_chunks(text):
+def get_conversation_chain(text):
     text_splitter = CharacterTextSplitter(
         separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
     )
-    chunks = text_splitter.split_text(text)
-    return chunks
+    text_chunks = text_splitter.split_text(text)
 
-
-def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-3-small",
-        api_key=st.session_state["cookies"]["OPENAI_API_KEY"],
-    )
+    if st.session_state["cookies"].get("model") == "GPT-4o-mini" and st.session_state["cookies"].get("OPENAI_API_KEY"):
+        embeddings = OpenAIEmbeddings(
+            model="text-embedding-3-small",
+            api_key=st.session_state["cookies"]["OPENAI_API_KEY"],
+        )
+        llm = ChatOpenAI(
+                model="gpt-4o-mini",
+                api_key=st.session_state["cookies"]["OPENAI_API_KEY"],
+            )
+    elif st.session_state["cookies"].get("model") == "Gemini-1.5" and st.session_state["cookies"].get("GOOGLE_API_KEY"):
+        embeddings = GoogleGenerativeAIEmbeddings(
+            model="models/embedding-001",
+            google_api_key=st.session_state["cookies"]["GOOGLE_API_KEY"]
+        )
+        llm = GoogleGenerativeAI(
+                    model="gemini-1.5-pro",
+                    api_key=st.session_state["cookies"]["GOOGLE_API_KEY"],
+                )
+    else:
+        st.error("You don't have access to the selected model. [Get access here](/get_access).")
+        return
+        
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
-
-
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI(
-        model="gpt-4o-mini",
-        api_key=st.session_state["cookies"]["OPENAI_API_KEY"],
-    )
 
     prompt = """
     You are a chatbot that answers the students questions about the context they provide which is a PDF file, and you will reply with markdown.
@@ -99,9 +106,9 @@ def main():
     utils.universal_setup(
         page_title="Ask My PDF", page_icon="🔍", upload_file_types=["pdf"]
     )
-    if st.session_state["cookies"].get("model") != "GPT-4o-mini":
-        st.error("Ask PDF is only supported by GPT-4o-mini model.")
-        st.stop()
+    #if st.session_state["cookies"].get("model") != "GPT-4o-mini":
+    #    st.error("Ask PDF is only supported by GPT-4o-mini model.")
+    #    st.stop()
 
     if (
         "conversation" not in st.session_state
@@ -120,7 +127,7 @@ def main():
         st.session_state.chat_history = []
 
     with st.sidebar:
-        if st.button("Process"):
+        if st.button("Process", use_container_width=True):
             try:
                 with st.spinner("Processing"):
                     # Extract text from pdf
@@ -129,14 +136,7 @@ def main():
                         page_range=None,
                     )
 
-                    # get the text chunks
-                    text_chunks = get_text_chunks(raw_text)
-
-                    # create vector store
-                    vectorstore = get_vectorstore(text_chunks)
-
-                    # creating a conversation chain
-                    st.session_state.conversation = get_conversation_chain(vectorstore)
+                    st.session_state.conversation = get_conversation_chain(raw_text)
 
                     st.success("Done! You can now start chatting.")
             except AttributeError:
